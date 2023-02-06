e6406bcf8dbd9e79d9056329d9e860ead43819f2
Bryan Jowers <553020+bryanjowers@users.noreply.github.com>
2023-02-05 19:32:44 -0600

    update OG

--- components/meta.tsx
+++ components/meta.tsx
@@ -34,7 +34,7 @@ const Meta = () => {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`useChainlinkFunctions() - quick and easy to understand code examples for Chainlink Functions. Powered by the Chainink community. Add your examples to this site.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
