fb6b00316f8e0e2fbabc1fce5ae86d70c26bd354
Bryan Jowers <553020+bryanjowers@users.noreply.github.com>
2023-02-05 21:48:44 -0600

    update color

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            What will you build with <br /> Chainlink Functions?
+            What will you build with <br /> <span className="text-[#375bd2]">Chainlink</span> Functions?
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
           <a
